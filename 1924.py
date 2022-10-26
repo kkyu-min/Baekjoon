7095ad42d9323539efb81c81d34799a3deeda7c7
kyumin-stack <lkm45454@naver.com>
2022-10-26 10:07:38 +0900

    2007년

---
+++ 1924.py
@@ -0,0 +1,17 @@
+# Title : 2007년
+# Date : 2022/10/26
+# https://www.acmicpc.net/problem/1924
+
+import sys 
+
+mon_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+week_list = ["SUN", "MON", "TUE", "WEN", "THU", "FRI", "SAT"]
+
+a,b = map(int, sys.stdin.readline().split())
+day = 0
+
+for i in range(a-1):
+  day += mon_list[i]
+day = (day + b) % 7
+
+print(week_list[day])
